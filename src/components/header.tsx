33fb55faab9b222f9f76613cdd1b5627297c8e20
rzt0317 <rzt0357@gmail.com>
2025-02-11 04:05:47 +0900

    feat: header 컴포넌트에 ssr 오류 관련 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,22 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { authStore } from "@/stores/auth-store";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
+  const userName = authStore.user?.name;
+  const userUserName = authStore.user?.username;
+
+  console.log(userName, userUserName);
   return (
     <nav className="bg-[rgba(251,251,253,0.8)] backdrop-blur-md fixed w-full z-50 border-b border-gray-200/80">
       <div className="container mx-auto px-4">
@@ -28,9 +40,16 @@ export default function Header() {
                 <Button
                   variant="ghost"
                   className="text-sm"
-                  onClick={() => router.push("/write")}
+                  onClick={() => router.push("/boards/write")}
                 >
                   글쓰기
+                </Button>
+                <Button
+                  variant="ghost"
+                  className="text-sm"
+                  onClick={() => router.push("/profile")}
+                >
+                  마이페이지
                 </Button>
                 <Button
                   variant="ghost"
