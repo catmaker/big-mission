6631af2fbeda5ba418903a2a73d726a96c9d1cad
rzt0317 <rzt0317@nate.com>
2025-02-13 04:28:05 +0900

    bug: 비밀번호 불일치 시 네트워크 오류가 아닌 서버에서 온 메시지 사용으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Providers } from "@/providers";
 export const metadata: Metadata = {
   title: "Bigs",
   description: "Bigs",
+  icons: {
+    icon: "/imgs/Logo.png",
+  },
 };
 
 export default function RootLayout({
